b48eca0b1138a6f77f173d5f20399fbab2915dc2
Talia Woodroof <taliawoodroof@gmail.com>
2026-03-14 12:08:24 +0000

    deps: add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,14 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.detekt)
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    detekt {
+        config.setFrom(files("${rootProject.projectDir}/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+    }
 }
